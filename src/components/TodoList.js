3476398c047d0a81f29aa0357dd30922d8f910c2
unknown <williamshty@gmail.com>
2019-03-12 00:56:50 +0800

    finished starter template with all css

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,19 @@
 import React from 'react'
 
 const TodoList = (props) => {
-    const { todoItems, deleteItem, editItem } = props
-    if(todoItems.length){
-        return (
-            todoItems.map(todo => {
-                return <li className="collection-item" key={todo.id}>
-                    <div>
-                        {todo.content}
-                        <div className="secondary-content">
-                           <i className="material-icons" onClick={()=>{editItem(todo)}}>edit</i>
-                           <i className="material-icons" onClick={()=>{deleteItem(todo.id)}}>delete</i>
-                        </div>
-                    </div>
-                </li>
-            })
-        )
-    } else return(
-        <li className="collection-item">
-            <h6 className="blue-text center">No more task left!</h6>
-          </li>
+    const { } = props
+    return (
+        <li className="collection-item" >
+            <div>
+                sample content
+                <div className="secondary-content">
+                    <i className="material-icons">edit</i>
+                    <i className="material-icons">delete</i>
+                </div>
+            </div>
+        </li>
     )
-    
+
 }
 
 export default TodoList
